050630e97345c8d11395d569e20fff8046e6a795
Juan Diego López <lopezjuandiego@gmail.com>
2021-11-23 17:48:57 -0300

    ants de flexbox

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require ("express");
+const path = require ("path");
+const app = express();
+app.set ("port",3000) 
+
+app.listen (app.get("port"), ()=> console.log("Server funcionando http://localhost:" + app.get('port'))); 
+
+app.use(express.static(path.resolve(__dirname, '../public'))); 
+
+app.get ("/", (req, res)=> {
+    res.sendFile(path.resolve(__dirname, './views/home.html'))});
+app.get ("/home", (req, res)=> {
+        res.sendFile(path.resolve(__dirname, './views/home.html'))});
+
