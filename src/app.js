a68e9841bed4c42f671f8daf09ba49dc8fd81a26
Juan Diego López <lopezjuandiego@gmail.com>
2021-12-07 21:21:14 -0300

    agregué header

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ app.use(express.static(path.resolve(__dirname, '../public')));
 
 app.get ("/", (req, res)=> {
     res.sendFile(path.resolve(__dirname, './views/home.html'))});
+
 app.get ("/home", (req, res)=> {
         res.sendFile(path.resolve(__dirname, './views/home.html'))});
 app.get ("/login", (req, res)=> {
